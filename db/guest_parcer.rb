d07de266613759ae46cc0c0343bf0e23d68b1e81
Shreya Patel <shreya317@gmail.com>
2015-07-20 15:34:50 -0500

    [RSVP-UI] created a parcer for guest login info

---
+++ db/guest_parcer.rb
@@ -0,0 +1,22 @@
+def parce_guest_login
+  Spreadsheet.client_encoding = 'UTF-8'
+  @workbook = Spreadsheet.open("../../../desktop/guestLogin.xls")
+
+  # Get the first worksheet in the Excel file
+  @worksheet = @workbook.worksheet(0)
+
+  # It can be a little tricky looping through the rows since the variable
+  # @worksheet.rows often seem to be empty, but this will work:
+  1.upto @worksheet.last_row_index do |index|
+    # .row(index) will return the row which is a subclass of Array
+    row = @worksheet.row(index)
+
+    @guest = Guest.new
+    #row[0] is the first cell in the current row, row[1] is the second cell, etc...
+    @guest.first_name = row[0]
+    @guest.last_name = row[1]
+    @guest.zip_code = row[2].to_i.to_s
+
+    @guest.save
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative "../db/guest_parcer.rb"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+parce_guest_login
